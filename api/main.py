1f8f5381e974d941b59a485b83542b965e718a02
Jorge Jimenez <environmentaladvocatejorge@gmail.com>
2024-11-28 21:32:15 -0500

    add middleware

--- api/main.py
+++ api/main.py
@@ -1,6 +1,7 @@
 import logging
 import os
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from mangum import Mangum
 from controllers.accounts_controller import create_accounts_controller
 from services.certificate_service import CertificateService
@@ -11,6 +12,18 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins, 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"], 
+)
 
 aws_region = os.getenv('AWS_REGION', 'us-east-1')
 secrets_repository = SecretsRepository(region_name=aws_region)
