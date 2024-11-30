b0f972e30dc6f0359bf1a66c135bd20d7d8bb890
Jorge Jimenez <environmentaladvocatejorge@gmail.com>
2024-11-29 22:40:42 -0500

    final fix

--- src/services/accountService.tsx
+++ src/services/accountService.tsx
@@ -7,13 +7,13 @@ export const fetchAccounts = async (
   getToken: () => string | null
 ): Promise<GetAccountsResponse> => {
   const token = getToken();
-  const response = await client.get<GetAccountsResponse>(
-    `/accounts?accessToken=${process.env.NEXT_PUBLIC_ACCESS_TOKEN}`,
-    {
-      headers: {
-        Authorization: token ? `Bearer ${token}` : undefined,
-      },
-    }
-  );
+  const response = await client.get<GetAccountsResponse>(`/accounts`, {
+    params: {
+      access_token: process.env.NEXT_PUBLIC_ACCESS_TOKEN,
+    },
+    headers: {
+      Authorization: token ? `Bearer ${token}` : undefined,
+    },
+  });
   return response.data;
 };
