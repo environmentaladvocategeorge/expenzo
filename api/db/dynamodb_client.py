ad2db51cecc1ac8e8b691d1f27a79e58ba638137
Jorge Jimenez <environmentaladvocatejorge@gmail.com>
2024-12-10 18:27:32 -0500

    fix runtime and dynamo permissions

--- api/db/dynamodb_client.py
+++ api/db/dynamodb_client.py
@@ -3,13 +3,8 @@ import os
 
 class DynamoDBClient:
     def __init__(self):
-        self.dynamodb = boto3.resource(
-            'dynamodb',
-            region_name=os.getenv('AWS_REGION'),
-            aws_access_key_id=os.getenv('AWS_ACCESS_KEY'),
-            aws_secret_access_key=os.getenv('AWS_SECRET_KEY'),
-        )
-        self.table = self.dynamodb.Table(os.getenv('DYNAMODB_TABLE'))
+        self.dynamodb = boto3.resource('dynamodb', region_name=os.getenv('AWS_REGION'))
+        self.table = self.dynamodb.Table(os.getenv('DYNAMO_DB_TABLE_NAME'))
 
     def get_table(self):
         return self.table
