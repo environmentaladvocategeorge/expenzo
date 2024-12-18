b8c67c180352d5c6922e2c2ac6e667d34c806e9a
Jorge Jimenez <environmentaladvocatejorge@gmail.com>
2024-12-17 19:35:48 -0500

    fix

--- api/services/scheduler_service.py
+++ api/services/scheduler_service.py
@@ -146,7 +146,7 @@ class SchedulerService:
                     Provider=account.Provider,
                     ProviderID=account.ProviderID,
                     EntityType="Transaction",
-                    EntityId=transaction_data['id'],
+                    EntityId=transaction_data.get('id'),
                     EntityData=transaction_data,
                     Timestamp=int(datetime.now(tz=timezone.utc).timestamp()),
                 )
